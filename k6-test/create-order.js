bbb8460bf960306a17a065efbc18658e5c44782e
ilhamhambali <ilhamhambali2000@gmail.com>
2025-10-01 12:51:19 +0700

    update main direktori

--- k6-test/create-order.js
+++ k6-test/create-order.js
@@ -2,11 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  // Simulate 200 concurrent users
   vus: 200,
-  // For a total duration of 30 seconds
   duration: '30s',
-  // Ramp up to 200 users over 10 seconds
   stages: [
     { duration: '10s', target: 200 },
     { duration: '20s', target: 200 },
@@ -16,12 +13,11 @@ export const options = {
 export default function () {
   const url = 'http://localhost:8080/orders';
 
-  // !!! IMPORTANT: Replace this with an ID of a product you created !!!
   const productId = '<YOUR_PRODUCT_ID>';
 
   const payload = JSON.stringify({
     productId: productId,
-    quantity: 1, // Order one item per request
+    quantity: 1, 
   });
 
   const params = {
@@ -37,5 +33,5 @@ export default function () {
     'order created successfully': (r) => r.json('id') !== '',
   });
 
-  sleep(1); // Wait 1 second between requests per VU
+  sleep(1); 
 }
